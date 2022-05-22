316f7fb6a9e394094f1e7dd391fbe88f3a1bfb97
yaena1223 <yaena1223@dgu.ac.kr>
2022-05-23 01:55:19 +0900

    0523 ossp2 moidfy information.py

--- information.py
+++ information.py
@@ -1,6 +1,8 @@
 name = input("이름을 입력하세요 : ")
 id = int(input("학번을 입력하세요 : "))
+school = input("대학교를 입력하세요 : ")
 
 print("\n<출력>")
 print("이름 : %s"%name)
 print("학번 : %s"%id)
+print("대학교 : %s"%school)
